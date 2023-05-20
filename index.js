85062f9bb801ae755d7f8af15da1beb92053a6e7
WillTDP <williamhazevoets@gmail.com>
2023-05-20 15:04:00 +0200

    FIX: the send webhook is now logged in console

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// Require express and body-parser
-const express = require("express")
-const bodyParser = require("body-parser")// Initialize express and define a port
-const app = express()
-const PORT = 3000// Tell express to use body-parser's JSON parsing
-app.use(bodyParser.json())// Start express on the defined port
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+const express = require("express");
+const multer = require("multer");
+
+const app = express();
+const PORT = 3000;
+
+// Set up multer middleware to handle multipart/form-data
+const upload = multer();
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// Endpoint for the webhook
+app.post("/hook", upload.any(), (req, res) => {
+  console.log(req.body); // Log the entire request body
+  res.status(200).end(); // Responding is important
+});
